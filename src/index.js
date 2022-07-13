dd3fe6c61e5e239d95cff3381c876dd64dceb3c8
kenz37 <ksushashaborkina@gmail.com>
2022-07-13 13:15:53 +0300

    feat: make task

--- src/index.js
+++ src/index.js
@@ -38,6 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    a = expr.match(/.{1,2}/g)
+
+    for (let i = 0; i < a.length; i++){
+        if (a[i] == 10){
+            a[i] = '.';
+        } else if (a[i]==11){
+            a[i] = '-';
+        }
+    }
+
+    let b = '';
+    let c = 0;
+    let d = [];
+
+    console.log(a);
+
+    for (let i = 0; i < a.length; i++){
+
+        if (a[i] != "00"){
+            b= b+ a[i];
+        }
+
+        c+=1;
+
+        if (c % 5==0 ){
+            d.push(b);
+            b='';
+        }
+    }
+
+    b = '';
+
+    console.log(d);
+
+    for (let i = 0; i < d.length; i++){
+        if (d[i] != '**********'){
+        d[i] = MORSE_TABLE[d[i]]} else{
+            d[i] = ' ';
+        }
+    }
+
+    console.log(d);
+
+
+    for (let i = 0; i < d.length; i++){
+        b = b+ d[i];
+    }
+
+    return b;
     // write your solution here
 }
 
